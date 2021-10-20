b39f07bd64ec5255ecab0d2b168e84d79adf6757
newengood <04-scones.card@newengood.com>
2021-10-19 17:03:57 -0700

    route testing 4

--- server.js
+++ server.js
@@ -19,15 +19,15 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker-db", { useNewUrlParser: true });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./index.html"))
+  res.sendFile(path.join(__dirname, "public/index.html"))
 });
 
 app.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname, "./stats.html"))
+  res.sendFile(path.join(__dirname, "public/stats.html"))
 });
 
 app.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, "./exercise.html"))
+  res.sendFile(path.join(__dirname, "public/exercise.html"))
 });
 
 
