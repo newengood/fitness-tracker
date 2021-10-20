a1d0894ab9d5fdef701971edaaa837c07abf4597
newengood <04-scones.card@newengood.com>
2021-10-19 17:02:19 -0700

    route testing 3

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,37 +16,21 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker-db");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker-db", { useNewUrlParser: true });
 
 app.get("/", (req, res) => {
-  db.Workout.find({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+  res.sendFile(path.join(__dirname, "./index.html"))
 });
 
-app.post("/api/workouts", (req, res) => {
-  db.Workout.find({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "./stats.html"))
 });
 
 app.get("/exercise", (req, res) => {
-  db.Workout.find({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+  res.sendFile(path.join(__dirname, "./exercise.html"))
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
